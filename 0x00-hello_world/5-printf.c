80289483af3d71cb24c0636c694f24cc97e9cb33
RomayLala <asune.rosemary@yahoo.com>
2024-06-22 15:32:16 -0700

    my c file commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
-int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); }
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
