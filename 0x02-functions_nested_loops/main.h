1b01db5a652ad72add6fe2ebd31b6f14eeb9a513
RomayLala <asune.rosemary@yahoo.com>
2024-06-25 10:56:12 -0700

    nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Included Libraries */
-#include <stdio.h> /* For NULL */
-
-/* Function Prototypes */
 int _putchar(char c);
-int _isalpha(int c);
+int print_sign(int n);
 
 #endif /* MAIN_H */
