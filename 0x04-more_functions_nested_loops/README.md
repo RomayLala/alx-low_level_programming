1cf0948ce9eaf893c031fb433f499a47eae80381
RomayLala <asune.rosemary@yahoo.com>
2024-06-27 14:53:21 -0700

    more nested loops commit

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -5,3 +5,4 @@ This function prints the numbers, from 0 to 9, followed by a new line.
 This function prints 10 times the numbers, from 0 to 14, followed by a new line.
 This function  draws a straight line in the terminal.
 This function  draws a diagonal line on the terminal.
+This function  prints a square, followed by a new line.
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Include any necessary headers here */
+
+/* Function prototype for _putchar */
 int _putchar(char c);
-void print_diagonal(int n);
+
+/* Function prototype for print_square */
+void print_square(int size);
+
+/* Any additional function prototypes can be added here */
 
 #endif /* MAIN_H */
