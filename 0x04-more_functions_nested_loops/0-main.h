2bd775d76ec6bbf3edb9c210a4554f21bdbd6752
RomayLala <asune.rosemary@yahoo.com>
2024-06-28 13:37:47 -0700

    more nested loops commit

---
+++ 0x04-more_functions_nested_loops/0-main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* 
+ * File: 0-main.h
+ * Auth: romay
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
