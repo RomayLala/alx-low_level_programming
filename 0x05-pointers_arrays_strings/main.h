db932e5d19b1f666c2223f81337e9a5cc5397559
RomayLala <asune.rosemary@yahoo.com>
2024-07-01 15:54:43 -0700

    pointers, arrays and strings commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototypes */
+/* Included Libraries */
+#include <unistd.h> /* For the _putchar function */
+
+/* Function Prototypes */
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
