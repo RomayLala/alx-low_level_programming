6bb27537aeb5684176209710d6114db64ae4c921
RomayLala <asune.rosemary@yahoo.com>
2024-07-03 07:54:13 -0700

    more pointers, array and strings commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	/* Find the end of the destination string */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* Append the source string to the destination string */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* Add the terminating null byte */
+	dest[i] = '\0';
+
+	return (dest);
+}
