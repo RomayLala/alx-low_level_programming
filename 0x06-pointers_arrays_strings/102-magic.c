4f7f05ac21f720095a60ff1e2e1c5d1ed6defede
RomayLala <asune.rosemary@yahoo.com>
2024-07-05 08:18:31 -0700

    more pointers, arrays and strings commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (indicating successful execution)
- */
 int main(void)
 {
-    int a[5] = {98, 402, 1024, 4096, 16384}; /* Array initialization */
-    int *p;                                  /* Pointer declaration */
+  int n;
+  int a[5];
+  int *p;
 
-    p = a;  /* Pointer p points to the first element of array a */
-
-    *(p + 2) = 98;  /* Update the third element of array a to 98 */
-    /* Update the third element of the array */
-    /* Prepare to print the updated value */
-    /* Step 3: Return 0 to indicate successful execution */
-    printf("a[2] = %d\n", *(p + 2));  /* Print the updated value of a[2] */
-
-    return (0);  /* Return 0 to indicate successful execution */
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
