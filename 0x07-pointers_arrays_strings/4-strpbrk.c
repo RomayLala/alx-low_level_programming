ca31eeac009261376b3923d97f852fbfc99a1cbf
RomayLala <asune.rosemary@yahoo.com>
2024-07-08 13:27:36 -0700

    more pointers, arrays and string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h> /* Include for NULL definition */
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be searched.
+ * @accept: The set of bytes to search for.
+ *
+ * Return: Pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+				return s;
+			a++;
+		}
+		s++;
+	}
+
+	return NULL; /* Ensure NULL is properly returned if no match is found */
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ This function  fills memory with a constant byte.
 This function  copies memory area.
 This function locates a character in a string.
 This function gets the length of a prefix substring.
+This function searches a string for any of a set of bytes.
