e6427ac4a7a6c7aab0d491fb704f2219ecc71b54
RomayLala <asune.rosemary@yahoo.com>
2024-07-22 05:46:21 -0700

    structures typedef commit

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,4 @@
 This prject defines a `struct dog` with the following attributes: - `name`: a pointer to a character string representing the dog's name. - `age`: a float representing the dog's age. - `owner`: a pointer to a character string representing the dog's owner.
 This function  initialize a variable of type struct dog.
 This function prints a struct dog and implementing a function `print_dog` to print the details of the dog in a formatted manner.
+This function defines a new type dog_t as a new name for the type struct dog.
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,22 +2,22 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - A structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
- * Description: This structure holds the basic information about a dog.
+ * Description: This structure represents a dog with a name, age, and owner.
  */
-struct dog
+typedef struct dog
 {
-char *name;
-float age;
-char *owner;
-};
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
 
-/* Function prototype */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+/* Function prototypes */
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 
 #endif /* DOG_H */
