be68e31472584acd430f34e8a3ac5199e436d4f3
RomayLala <asune.rosemary@yahoo.com>
2024-08-06 10:22:16 -0700

    more singly linked list commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Frees a listint_t list and sets the head to NULL.
+ * @head: Double pointer to the head of the list.
+ *
+ * Description: The function iterates through the list, freeing each node.
+ * After freeing all nodes, it sets the head to NULL.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
