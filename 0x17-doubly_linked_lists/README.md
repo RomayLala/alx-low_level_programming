62f37d1b70de460bc54cac731bde847e47033e5b
RomayLala <asune.rosemary@yahoo.com>
2024-09-19 11:41:21 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -1,1 +1,2 @@
 This function prints all the elements of a dlistint_t list.
+This function returns the number of elements in a linked dlistint_t list.
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -11,7 +11,6 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
  */
 typedef struct dlistint_s
 {
@@ -20,6 +19,7 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+size_t dlistint_len(const dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
